035b483520924939f1445f52db987391162265a5
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2024-04-06 17:03:12 +0700

    Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import Home from "@/views/HomeView.vue";
+import About from "@/views/AboutView.vue";
+import Manage from '@/views/ManageView.vue';
+
+const routes = [
+  {
+    name: 'home',
+    path: "/",
+    component: Home,
+  },
+
+  {
+    name: 'about',
+    path: "/about",
+    component: About,
+  },
+  {
+    name: 'manage',
+    alias: '/manage',
+    path: "/manage-music",
+    component: Manage,
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  linkActiveClass: 'text-yellow-500'
 })
+
+router.beforeEach((to, from, next) => {
+  console.log('Global Guard');
+  console.log(to, from);
+
+  next()
+});
 
 export default router
