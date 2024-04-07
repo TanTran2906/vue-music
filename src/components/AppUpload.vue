4fdf3dfd554d63c7581e11d755026ee35c74f06a
TanTran2906 <86616223+TanTran2906@users.noreply.github.com>
2024-04-08 00:20:14 +0700

    Edit a song

--- src/components/AppUpload.vue
+++ src/components/AppUpload.vue
@@ -19,7 +19,9 @@
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload($event)" />
       <hr class="my-6" />
+
       <!-- Progess Bars -->
       <div class="mb-4" v-for="upload in uploads" :key="upload.name">
         <!-- File Name -->
@@ -55,7 +57,7 @@ export default {
       this.is_dragover = false
       // console.log($event)
 
-      const files = [...$event.dataTransfer.files]
+      const files = $event.dataTransfer ? [...$event.dataTransfer.files] : [...$event.target.files]
       files.forEach((file) => {
         if (file.type !== 'audio/mpeg') {
           return
@@ -107,6 +109,11 @@ export default {
       })
       // console.log(files)
     }
+  },
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel()
+    })
   }
 }
 </script>
